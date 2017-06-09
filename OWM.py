6250f658783f4ffe9adbe2539365cbd03a640cf6
claire mitchell <clairelouisemitchell1@gmail.com>
2017-06-09 13:41:35 +0100

    revised version using API

---
+++ OWM.py
@@ -0,0 +1,23 @@
+from urllib2 import urlopen
+import requests
+import json
+from prettytable import PrettyTable
+
+response = requests.get("http://api.openweathermap.org/data/2.5/forecast?q=Bristol,uk&appid=1ac5db62be3852d13313466234712716")
+
+response = json.loads(response.text)
+
+y = PrettyTable()
+dates = []
+temps = []
+for x in response['list']:
+	dates.append( x['dt_txt'])
+	temps.append(x['main']['temp']-273.15)
+
+	temps. (temps - 273)
+
+y.add_column("Date", dates)
+y.add_column("Temperature", temps)
+
+print y
+
